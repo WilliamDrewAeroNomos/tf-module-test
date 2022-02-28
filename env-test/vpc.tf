307bac7b2691b9d91a9e732606b87e4e4cdbfdb6
William Drew <wdrew@governmentcio.com>
2022-02-28 14:35:17 -0500

    Added test environment

---
+++ env-test/vpc.tf
@@ -0,0 +1,14 @@
+#---------------------------
+# Development VPC
+#---------------------------
+
+module "ahroc_main_vpc" {
+
+  source = "git::https://github.com/WilliamDrewAeroNomos/tf-modules.git//modules/vpc?ref=v1.1"
+
+  # insert required variables here
+
+  _cidr_block = var.CIDR_BLOCK
+  _name       = var.VPC_NAME
+
+}
