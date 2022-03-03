cf88b157402cd82fca4121f7e0d2f2ab3e6e0201
William Drew <wdrew@governmentcio.com>
2022-03-03 15:37:34 -0500

    Reorganized stage environment

--- env-stage/network/main.tf
+++ env-stage/network/main.tf
@@ -1,10 +1,10 @@
 #---------------------------
-# Staging environment
+# Development environment
 #---------------------------
 
 module "ahroc_main_vpc" {
 
-  source = "git::https://github.com/WilliamDrewAeroNomos/tf-modules.git//modules/vpc?ref=v1.2"
+  source = "git::https://github.com/WilliamDrewAeroNomos/tf-modules.git//modules/vpc?ref=v2.0.0"
 
   # insert required variables here
 
