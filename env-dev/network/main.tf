988a5b3f69d38e9172d0c46ade9218d2b3fbeb93
William Drew <wdrew@governmentcio.com>
2022-03-04 13:42:37 -0500

    Added ES domain

--- env-dev/network/main.tf
+++ env-dev/network/main.tf
@@ -6,6 +6,14 @@ data "aws_availability_zones" "available" {
   state = "available"
 }
 
+locals {
+  elk_domain = "${var.ENVIRONMENT}-elk-domain"
+}
+
+data "aws_region" "current" {}
+
+data "aws_caller_identity" "current" {}
+
 module "ahroc_main_vpc" {
 
   #source = "git::https://github.com/WilliamDrewAeroNomos/tf-modules.git//modules/vpc?ref=v2.0.0"
@@ -213,19 +221,105 @@ resource "aws_route_table" "nated_3" {
 # Associations for NAT'd subnets and route tables
 
 resource "aws_route_table_association" "nated_1" {
-    subnet_id = aws_subnet.nated_1.id
-    route_table_id = aws_route_table.nated_1.id
+  subnet_id      = aws_subnet.nated_1.id
+  route_table_id = aws_route_table.nated_1.id
 }
 
 resource "aws_route_table_association" "nated_2" {
-    subnet_id = aws_subnet.nated_2.id
-    route_table_id = aws_route_table.nated_2.id
+  subnet_id      = aws_subnet.nated_2.id
+  route_table_id = aws_route_table.nated_2.id
 }
 
 resource "aws_route_table_association" "nated_3" {
-    subnet_id = aws_subnet.nated_3.id
-    route_table_id = aws_route_table.nated_3.id
+  subnet_id      = aws_subnet.nated_3.id
+  route_table_id = aws_route_table.nated_3.id
 }
 
+# Service linked role
 
+resource "aws_iam_service_linked_role" "es" {
+  aws_service_name = "es.amazonaws.com"
+}
 
+# ES domain
+
+resource "aws_elasticsearch_domain" "es" {
+  domain_name           = local.elk_domain
+  elasticsearch_version = "7.7"
+
+  cluster_config {
+    instance_count         = 3
+    instance_type          = "r5.large.elasticsearch"
+    zone_awareness_enabled = true
+
+    zone_awareness_config {
+      availability_zone_count = 3
+    }
+  }
+
+  vpc_options {
+    subnet_ids = [
+      aws_subnet.nated_1.id,
+      aws_subnet.nated_2.id,
+      aws_subnet.nated_3.id
+    ]
+
+    security_group_ids = [
+      aws_security_group.es.id
+    ]
+  }
+
+  ebs_options {
+    ebs_enabled = true
+    volume_size = 10
+  }
+
+  access_policies = <<CONFIG
+{
+  "Version": "2012-10-17",
+  "Statement": [
+      {
+          "Action": "es:*",
+          "Principal": "*",
+          "Effect": "Allow",
+          "Resource": "arn:aws:es:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:domain/${local.elk_domain}/*"
+      }
+  ]
+}
+  CONFIG
+
+  snapshot_options {
+    automated_snapshot_start_hour = 23
+  }
+
+  tags = {
+    Domain = local.elk_domain
+  }
+}
+
+# ES security group
+
+resource "aws_security_group" "es" {
+  name = "${var.ENVIRONMENT}-es-sg"
+  description = "Allow inbound traffic to ElasticSearch from VPC CIDR"
+  vpc_id = module.ahroc_main_vpc.vpc-main-id
+
+  ingress {
+      from_port = 0
+      to_port = 0
+      protocol = "-1"
+      cidr_blocks = [
+          module.ahroc_main_vpc.vpc-cidr-block
+      ]
+  }
+}
+
+# Outputs
+
+output "elk_endpoint" {
+  value = aws_elasticsearch_domain.es.endpoint
+}
+
+output "elk_kibana_endpoint" {
+  value = aws_elasticsearch_domain.es.kibana_endpoint
+}
