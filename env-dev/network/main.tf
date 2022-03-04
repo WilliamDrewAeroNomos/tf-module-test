59e5444cccb0fe5d1c87f3fd2e6ba4170c9f6d5e
William Drew <wdrew@governmentcio.com>
2022-03-04 11:39:13 -0500

    Added NAT'd subnets

--- env-dev/network/main.tf
+++ env-dev/network/main.tf
@@ -53,16 +53,16 @@ resource "aws_subnet" "main-public-3" {
   }
 }
 
-resource "aws_subnet" "main-public-4" {
-  vpc_id                  = module.ahroc_main_vpc.vpc-main-id
-  cidr_block              = cidrsubnet(module.ahroc_main_vpc.vpc-cidr-block, 8, 3)
-  map_public_ip_on_launch = "true"
-  availability_zone       = data.aws_availability_zones.available.names[3]
-
-  tags = {
-    Name = "${var.ENVIRONMENT}-public-subnet-${data.aws_availability_zones.available.names[3]}"
-  }
-}
+#resource "aws_subnet" "main-public-4" {
+#  vpc_id                  = module.ahroc_main_vpc.vpc-main-id
+#  cidr_block              = cidrsubnet(module.ahroc_main_vpc.vpc-cidr-block, 8, 3)
+#  map_public_ip_on_launch = "true"
+#  availability_zone       = data.aws_availability_zones.available.names[3]
+#
+#  tags = {
+#    Name = "${var.ENVIRONMENT}-public-subnet-${data.aws_availability_zones.available.names[3]}"
+#  }
+#}
 
 # Internet Gateway
 
@@ -77,33 +77,66 @@ resource "aws_internet_gateway" "igw-main" {
 # Route table
 
 resource "aws_route_table" "public" {
-    vpc_id = module.ahroc_main_vpc.vpc-main-id
+  vpc_id = module.ahroc_main_vpc.vpc-main-id
 
-    route {
-        cidr_block = "0.0.0.0/0"
-        gateway_id = aws_internet_gateway.igw-main.id
-    }
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw-main.id
+  }
 
-    tags = {
-        Name = "${var.ENVIRONMENT}-public-rt"
-    }
+  tags = {
+    Name = "${var.ENVIRONMENT}-public-rt"
+  }
 }
 
 # Route table associations
 
 resource "aws_route_table_association" "public_1" {
-    subnet_id = aws_subnet.main-public-1.id
-    route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.main-public-1.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "public_2" {
-    subnet_id = aws_subnet.main-public-2.id
-    route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.main-public-2.id
+  route_table_id = aws_route_table.public.id
 }
 
 resource "aws_route_table_association" "public_3" {
-    subnet_id = aws_subnet.main-public-3.id
-    route_table_id = aws_route_table.public.id
+  subnet_id      = aws_subnet.main-public-3.id
+  route_table_id = aws_route_table.public.id
 }
+
+# NAT'd subnets
+
+resource "aws_subnet" "nated_1" {
+  vpc_id            = module.ahroc_main_vpc.vpc-main-id
+  cidr_block        = cidrsubnet(module.ahroc_main_vpc.vpc-cidr-block, 8, 4)
+  availability_zone = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "${var.ENVIRONMENT}-nated-subnet-${data.aws_availability_zones.available.names[0]}"
+  }
+}
+
+resource "aws_subnet" "nated_2" {
+  vpc_id            = module.ahroc_main_vpc.vpc-main-id
+  cidr_block        = cidrsubnet(module.ahroc_main_vpc.vpc-cidr-block, 8, 5)
+  availability_zone = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name = "${var.ENVIRONMENT}-nated-subnet-${data.aws_availability_zones.available.names[1]}"
+  }
+}
+
+resource "aws_subnet" "nated_3" {
+  vpc_id            = module.ahroc_main_vpc.vpc-main-id
+  cidr_block        = cidrsubnet(module.ahroc_main_vpc.vpc-cidr-block, 8, 6)
+  availability_zone = data.aws_availability_zones.available.names[2]
+
+  tags = {
+    Name = "${var.ENVIRONMENT}-nated-subnet-${data.aws_availability_zones.available.names[2]}"
+  }
+}
+
 
 
