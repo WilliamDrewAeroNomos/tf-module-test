9b5a0af2ca729549c84301383182a7730fc75c9f
William Drew <wdrew@governmentcio.com>
2022-03-04 11:24:49 -0500

    Added route table associations

--- env-dev/network/main.tf
+++ env-dev/network/main.tf
@@ -89,4 +89,21 @@ resource "aws_route_table" "public" {
     }
 }
 
+# Route table associations
+
+resource "aws_route_table_association" "public_1" {
+    subnet_id = aws_subnet.main-public-1.id
+    route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_2" {
+    subnet_id = aws_subnet.main-public-2.id
+    route_table_id = aws_route_table.public.id
+}
+
+resource "aws_route_table_association" "public_3" {
+    subnet_id = aws_subnet.main-public-3.id
+    route_table_id = aws_route_table.public.id
+}
+
 
