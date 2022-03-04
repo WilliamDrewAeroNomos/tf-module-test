1e51ddf158173f727f0a55b1b0a8932160786c2d
William Drew <wdrew@governmentcio.com>
2022-03-04 10:22:55 -0500

    Added public subnets

--- env-dev/network/main.tf
+++ env-dev/network/main.tf
@@ -2,9 +2,14 @@
 # Development environment
 #---------------------------
 
+data "aws_availability_zones" "available" {
+  state = "available"
+}
+
 module "ahroc_main_vpc" {
 
-  source = "git::https://github.com/WilliamDrewAeroNomos/tf-modules.git//modules/vpc?ref=v2.0.0"
+  #source = "git::https://github.com/WilliamDrewAeroNomos/tf-modules.git//modules/vpc?ref=v2.0.0"
+  source = "../../../tf-modules/modules/vpc"
 
   # insert required variables here
 
@@ -13,3 +18,48 @@ module "ahroc_main_vpc" {
 
 }
 
+# Subnets
+
+resource "aws_subnet" "main-public-1" {
+  vpc_id                  = module.ahroc_main_vpc.vpc-main-id
+  cidr_block              = cidrsubnet(module.ahroc_main_vpc.vpc-cidr-block, 8, 0)
+  map_public_ip_on_launch = "true"
+  availability_zone       = data.aws_availability_zones.available.names[0]
+
+  tags = {
+    Name = "${var.ENVIRONMENT}-public-subnet-${data.aws_availability_zones.available.names[0]}"
+  }
+}
+
+resource "aws_subnet" "main-public-2" {
+  vpc_id                  = module.ahroc_main_vpc.vpc-main-id
+  cidr_block              = cidrsubnet(module.ahroc_main_vpc.vpc-cidr-block, 8, 1)
+  map_public_ip_on_launch = "true"
+  availability_zone       = data.aws_availability_zones.available.names[1]
+
+  tags = {
+    Name = "${var.ENVIRONMENT}-public-subnet-${data.aws_availability_zones.available.names[1]}"
+  }
+}
+
+resource "aws_subnet" "main-public-3" {
+  vpc_id                  = module.ahroc_main_vpc.vpc-main-id
+  cidr_block              = cidrsubnet(module.ahroc_main_vpc.vpc-cidr-block, 8, 2)
+  map_public_ip_on_launch = "true"
+  availability_zone       = data.aws_availability_zones.available.names[2]
+
+  tags = {
+    Name = "${var.ENVIRONMENT}-public-subnet-${data.aws_availability_zones.available.names[2]}"
+  }
+}
+
+resource "aws_subnet" "main-public-4" {
+  vpc_id                  = module.ahroc_main_vpc.vpc-main-id
+  cidr_block              = cidrsubnet(module.ahroc_main_vpc.vpc-cidr-block, 8, 3)
+  map_public_ip_on_launch = "true"
+  availability_zone       = data.aws_availability_zones.available.names[3]
+
+  tags = {
+    Name = "${var.ENVIRONMENT}-public-subnet-${data.aws_availability_zones.available.names[3]}"
+  }
+}
