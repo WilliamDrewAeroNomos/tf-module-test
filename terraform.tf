0974dcc3334d7702681e5d40030e79b79764f953
William Drew <wdrew@governmentcio.com>
2022-03-09 18:20:23 -0500

    Consolidated variables

--- terraform.tf
+++ terraform.tf
@@ -94,17 +94,6 @@ variable "AUTO_SHUTDOWN_SCHEDULE" {
   default     = "0000-1200 UTC"
 }
 
-variable "region" {
-  default = "us-east-1"
-}
-
-variable "app_version" {
-  default = "1.0.0"
-}
-
-variable "stage" {
-  default = "dev"
-}
 
 variable "resource_name" {
   default = "number"
