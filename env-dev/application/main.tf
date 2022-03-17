2f7abd5d06febd9c65ac37568c69adaa3747e8ab
William Drew <wdrew@governmentcio.com>
2022-03-17 14:29:39 -0400

    Changed scaling min to 10 and max to 50

--- env-dev/application/main.tf
+++ env-dev/application/main.tf
@@ -2,6 +2,10 @@
 #
 #
 
+# ------------------------------------------
+# AMI created via Packer and retrieved via data API
+# ------------------------------------------
+
 data "aws_ami" "nodejs-image" {
   most_recent = true
   owners      = ["self"]
@@ -11,10 +15,18 @@ data "aws_ami" "nodejs-image" {
   }
 }
 
+# ------------------------------------------
+# Access key to front-end EC2 instances
+# ------------------------------------------
+
 resource "aws_key_pair" "ahroc-front-end-key" {
   key_name   = "ahroc-front-end-key"
   public_key = file(var.PATH_TO_PUBLIC_KEY)
 }
+
+# ------------------------------------------
+# Front-end security group
+# ------------------------------------------
 
 resource "aws_security_group" "ahroc-front-end-sg" {
   vpc_id      = data.terraform_remote_state.network.outputs.vpc-main-id
@@ -41,6 +53,10 @@ resource "aws_security_group" "ahroc-front-end-sg" {
   }
 }
 
+# ------------------------------------------
+# Elastic load balancer security group
+# ------------------------------------------
+
 resource "aws_security_group" "ahroc-front-end-elb-sg" {
   vpc_id      = data.terraform_remote_state.network.outputs.vpc-main-id
   name        = "ahroc-front-end-elb-sg"
@@ -64,6 +80,10 @@ resource "aws_security_group" "ahroc-front-end-elb-sg" {
     Name = "ahroc-front-end-elb-sg"
   }
 }
+
+# ------------------------------------------
+# Elastic load balancer
+# ------------------------------------------
 
 resource "aws_elb" "ahroc-front-end-elb" {
   name = "ahroc-front-end-elb"
@@ -97,6 +117,10 @@ resource "aws_elb" "ahroc-front-end-elb" {
   }
 }
 
+# ------------------------------------------
+# Launch configuration
+# ------------------------------------------
+
 resource "aws_launch_configuration" "ahroc-front-end-launch-configuration" {
   name_prefix     = "ahroc-front-end-launch-configuration"
   image_id        = data.aws_ami.nodejs-image.id
@@ -105,13 +129,17 @@ resource "aws_launch_configuration" "ahroc-front-end-launch-configuration" {
   security_groups = [aws_security_group.ahroc-front-end-sg.id]
 }
 
+# ------------------------------------------
+# Auto-scaling group
+# ------------------------------------------
+
 resource "aws_autoscaling_group" "ahroc-front-end-autoscaling" {
   name = "ahroc-front-end-autoscaling"
   vpc_zone_identifier = [element(data.terraform_remote_state.network.outputs.public_subnet_ids, 0),
   element(data.terraform_remote_state.network.outputs.public_subnet_ids, 1)]
   launch_configuration      = aws_launch_configuration.ahroc-front-end-launch-configuration.name
-  min_size                  = 1
-  max_size                  = 2
+  min_size                  = 10
+  max_size                  = 50
   health_check_grace_period = 300
   health_check_type         = "EC2"
   load_balancers            = [aws_elb.ahroc-front-end-elb.name]
@@ -119,7 +147,7 @@ resource "aws_autoscaling_group" "ahroc-front-end-autoscaling" {
 
   tag {
     key                 = "Name"
-    value               = "AHROC Front-END ELB"
+    value               = "AHROC front end"
     propagate_at_launch = true
   }
 }
