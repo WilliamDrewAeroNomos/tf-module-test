870f14fadf06cd3b4965838bcd3804aaa7722c1b
William Drew <wdrew@governmentcio.com>
2022-03-17 10:31:16 -0400

    Added aws-ami data block

--- env-dev/application/main.tf
+++ env-dev/application/main.tf
@@ -2,3 +2,16 @@
 #
 #
 
+data "aws_ami" "nodejs-image" {
+  most_recent = true
+  owners      = ["self"]
+  filter {
+    name   = "tag:component-name"
+    values = ["ahroc-nodejs-image"]
+  }
+}
+
+output "ahroc-nodejs-image-id" {
+  value = data.aws_ami.nodejs-image.id
+}
+
