4b279d968e19a78c2cfe7a9c5cf6325295430ecb
William Drew <wdrew@governmentcio.com>
2022-03-17 11:10:27 -0400

    Added elb security group and attached to ELB security group

--- env-dev/application/main.tf
+++ env-dev/application/main.tf
@@ -11,7 +11,26 @@ data "aws_ami" "nodejs-image" {
   }
 }
 
-output "ahroc-nodejs-image-id" {
-  value = data.aws_ami.nodejs-image.id
-}
+resource "aws_security_group" "ahroc-front-end-elb-securitygroup" {
+  vpc_id      = data.terraform_remote_state.network.outputs.vpc-main-id
+  name        = "ahroc-front-end-elb-sg"
+  description = "Security group for the AHROC front end nodes"
 
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 80
+    to_port     = 8081
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "ahroc-front-end-elb-sg"
+  }
+}
--- env-dev/application/outputs.tf
+++ env-dev/application/outputs.tf
@@ -1,3 +1,11 @@
 
 # Outputs
 
+
+output "ahroc-nodejs-image-id" {
+  value = data.aws_ami.nodejs-image.id
+}
+
+output "ahroc-front-end-elb-sg-id" {
+  value = aws_security_group.ahroc-front-end-elb-securitygroup.id
+}
