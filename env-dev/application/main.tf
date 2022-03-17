798f63f8a782c882b07f1171a3b3bc9a9240cbe5
William Drew <wdrew@governmentcio.com>
2022-03-17 11:30:55 -0400

    Added front end ELB

--- env-dev/application/main.tf
+++ env-dev/application/main.tf
@@ -34,3 +34,33 @@ resource "aws_security_group" "ahroc-front-end-elb-securitygroup" {
     Name = "ahroc-front-end-elb-sg"
   }
 }
+
+resource "aws_elb" "ahroc-front-end-elb" {
+  name            = "ahroc-front-end-elb"
+  subnets         = [data.terraform_remote_state.network.outputs.public_subnet_ids[0], 
+  									data.terraform_remote_state.network.outputs.public_subnet_ids[1]]
+  security_groups = [aws_security_group.ahroc-front-end-elb-securitygroup.id]
+
+  listener {
+    instance_port     = 8080
+    instance_protocol = "http"
+    lb_port           = 80
+    lb_protocol       = "http"
+  }
+
+  health_check {
+    healthy_threshold   = 2
+    unhealthy_threshold = 2
+    timeout             = 3
+    target              = "HTTP:8080/login"
+    interval            = 30
+  }
+
+  cross_zone_load_balancing   = true
+  connection_draining         = true
+  connection_draining_timeout = 400
+
+  tags = {
+    Name = "ahroc_front_end_sg_elb"
+  }
+}
