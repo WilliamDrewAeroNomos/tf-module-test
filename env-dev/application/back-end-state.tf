dfbc41bcc7521aa8b2feab250a251b08bb398014
William Drew <wdrew@governmentcio.com>
2022-03-17 16:17:46 -0400

    Commments on provider usage

--- env-dev/application/back-end-state.tf
+++ env-dev/application/back-end-state.tf
@@ -1,4 +1,12 @@
 
+# ----------------------------------------------
+# Back end configuration stored in S3 bucket
+# Note: "terraform.backend.key must match
+#       the environment (i.e. dev, test, 
+#			  stage, etc.) and the layer (application,
+#				persistence, network, etc.)
+# ----------------------------------------------
+
 terraform {
   backend "s3" {
     bucket         = "dod-usarmy-tradoc-cmh-arhoc-terraform-state"
