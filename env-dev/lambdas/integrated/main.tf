60efc50d92f3474448938c6459932a842300daa0
William Drew <wdrew@governmentcio.com>
2022-03-21 11:29:44 -0400

    First version of 1 API GW to multiple lambda functions

---
+++ env-dev/lambdas/integrated/main.tf
@@ -0,0 +1,40 @@
+
+data "archive_file" "lambda_hello_world" {
+  type = "zip"
+
+  source_dir  = "${path.module}/hello-world"
+  output_path = "${path.module}/hello-world.zip"
+}
+
+resource "aws_iam_role" "lambda_exec" {
+  name = "serverless_lambda"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action = "sts:AssumeRole"
+      Effect = "Allow"
+      Sid    = ""
+      Principal = {
+        Service = "lambda.amazonaws.com"
+      }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy" {
+  role       = aws_iam_role.lambda_exec.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "lambda_functions" {
+  for_each         = var.lambdas
+  filename         = "hello-world.zip"
+  function_name    = each.value.name
+  role             = aws_iam_role.lambda_exec.arn
+  handler          = "index.handler"
+  source_code_hash = filebase64sha256("hello-world.zip")
+
+  runtime = "nodejs14.x"
+}
