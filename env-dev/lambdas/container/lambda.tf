c4743425888829d6c6ede9a1a376b6843ffc1da0
William Drew <wdrew@governmentcio.com>
2022-03-25 16:48:07 -0400

    Removed extraneous policy code

--- env-dev/lambdas/container/lambda.tf
+++ env-dev/lambdas/container/lambda.tf
@@ -64,46 +64,6 @@ resource "aws_iam_role" "iam_role" {
   assume_role_policy = data.aws_iam_policy_document.lambda_assume_role_policy.json
 }
 
-data "aws_iam_policy_document" "lambda" {
-  statement {
-    actions = [
-      "logs:CreateLogGroup",
-      "logs:CreateLogStream",
-      "logs:PutLogEvents"
-    ]
-    effect    = "Allow"
-    resources = ["*"]
-    sid       = "CreateCloudWatchLogs"
-  }
-
-  statement {
-    actions = [
-      "codecommit:GitPull",
-      "codecommit:GitPush",
-      "codecommit:GitBranch",
-      "codecommit:ListBranches",
-      "codecommit:CreateCommit",
-      "codecommit:GetCommit",
-      "codecommit:GetCommitHistory",
-      "codecommit:GetDifferences",
-      "codecommit:GetReferences",
-      "codecommit:BatchGetCommits",
-      "codecommit:GetTree",
-      "codecommit:GetObjectIdentifier",
-      "codecommit:GetMergeCommit"
-    ]
-    effect    = "Allow"
-    resources = ["*"]
-    sid       = "CodeCommit"
-  }
-}
-
-resource "aws_iam_policy" "lambda" {
-  name   = "${local.prefix}-lambda-policy"
-  path   = "/"
-  policy = data.aws_iam_policy_document.lambda.json
-}
-
 resource "aws_lambda_function" "lambda_function" {
   depends_on = [
     null_resource.ecr_image
