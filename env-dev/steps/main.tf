488cd52445d35f6be497f48feade07ee5f432303
William Drew <wdrew@governmentcio.com>
2022-03-29 15:55:16 -0400

    Clean up before release cut

--- env-dev/steps/main.tf
+++ env-dev/steps/main.tf
@@ -140,6 +140,7 @@ resource "aws_iam_role" "iam_for_lambda" {
 }
 EOF
 }
+
 #------------------------------------------------------
 # Create state machine using lambdas
 #------------------------------------------------------
